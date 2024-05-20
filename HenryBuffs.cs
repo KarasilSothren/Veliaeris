eb3b0c839ce6ab0eec25b8b5688c0f9cb97152b8
KarasilSothren <44912430+KarasilSothren@users.noreply.github.com>
2024-05-19 20:55:47 -0500

    Add files via upload

---
+++ HenryBuffs.cs
@@ -0,0 +1,28 @@
+﻿using RoR2;
+using UnityEngine;
+
+namespace HenryMod.Survivors.Henry
+{
+    public static class VeliaerisBuffs
+    {
+        // armor buff gained during roll
+        public static BuffDef armorBuff;
+        public static BuffDef abyss;
+
+        public static void Init(AssetBundle assetBundle)
+        {
+            armorBuff = Modules.Content.CreateAndAddBuff("HenryArmorBuff",
+                LegacyResourcesAPI.Load<BuffDef>("BuffDefs/HiddenInvincibility").iconSprite,
+                Color.white,
+                false,
+                false);
+
+            abyss = Modules.Content.CreateAndAddBuff("abyss",
+                LegacyResourcesAPI.Load<BuffDef>("BuffDefs/HiddenInvincibility").iconSprite,
+                Color.magenta,
+                true,
+                true);
+
+        }
+    }
+}
